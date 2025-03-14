ff8baea3a416bee05d2b21b9074463d4b09dab30
sejin <sejin@DESKTOP-ICG4EP2>
2025-03-14 15:23:56 +0900

    chore: 백엔드 초기 세팅

---
+++ src/main/java/com/eight/travel/MytravelApplication.java
@@ -0,0 +1,15 @@
+package com.eight.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:config/secu.properties")
+public class MytravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eight/travel/MytravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eight.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
